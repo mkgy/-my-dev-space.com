d084c1271b5d3f32b7241d7511f4340a0706d1cc
mkgy <mk.gyawu@gmail.com>
2024-03-28 22:25:45 +0000

    commited GMAIL feature second time

--- resources/views/test-mail.blade.php
+++ resources/views/test-mail.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Mail</title>
+    <title>Laravel Mail:</title>
 </head>
 <body>
+
     <h1>{{ $data['title'] }}</h1>
     <p>{{ $data['body'] }}</p>
     <p>Thank you</p>
